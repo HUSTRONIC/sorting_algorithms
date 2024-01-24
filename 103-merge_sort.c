916836c834ead8b2025e8e1e13e7383f9f6171a7
HUSTRONIC <ssesanhgahussein.sh@gmail.com>
2024-01-24 14:16:12 -0800

    merge

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -14,10 +14,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return ('\0');
 	p = malloc(nmemb * size);
-	if (p == '\0')
+	if (p == 0)
 		return ('\0');
 	for (i = 0; i < (nmemb * size); i++)
-		p[i] = '\0';
+		p[i] = 0;
 	return (p);
 }
 /**
